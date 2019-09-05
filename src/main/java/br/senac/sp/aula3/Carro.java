0b40d0df8332a7ccfefb8ab7201a14ad5f3d3cdd
ROGERIO SARTORI LUCON <rogerio.slucon@senacsp.edu.br>
2019-09-05 09:09:52 -0300

    Tarefa 1

--- src/main/java/br/senac/sp/aula3/Carro.java
+++ src/main/java/br/senac/sp/aula3/Carro.java
@@ -10,5 +10,80 @@ package br.senac.sp.aula3;
  * @author fernando.fernandes
  */
 public class Carro {
+    private static String fabricante;
+    private String[] rodas;
+    private String cor;
+    private String modelo;
+    private int velocidade;
+
+    public Carro(String color) {
+        cor = color;
+    }
     
+    public void descreve(){
+        String aux = "";
+        if(!fabricante.equals("")){
+            aux += "Um " + fabricante;
+        }
+        if(!modelo.equals("")){
+            aux += " " + modelo;
+        }
+        if(!cor.equals("")){
+            aux += " de cor " + cor;
+        }
+        System.out.println(aux);
+        
+    }
+    
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = formataTextoUpper(fabricante);
+    }
+
+    public String[] getRodas() {
+        return rodas;
+    }
+
+    public void setRodas(String[] rodas) {
+        this.rodas = rodas;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = formataTextoLower(cor);
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = formataTextoUpper(modelo);
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+    
+    private String formataTextoUpper(String txt){
+        String aux = txt.substring(0,1).toUpperCase()+txt.substring(1);
+    
+    return aux;
+    }
+    
+    private String formataTextoLower(String txt){
+        String aux = txt.toLowerCase();
+    
+    return aux;
+    }
 }
